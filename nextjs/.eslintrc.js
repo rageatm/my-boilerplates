5ffd04a13d57a767098752e9619b4f1bb68cc7a1
Han Il-u <rageatm@alumni.kaist.ac.kr>
2023-09-14 13:21:39 +0900

    feat: tsconfig 수정

--- nextjs/.eslintrc.js
+++ nextjs/.eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  env: { browser: true, node: true },
-  extends: [
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: { 'react/react-in-jsx-scope': 'off' },
+  extends: ['next/core-web-vitals', '@rageatm/eslint-config/react-ts'],
 };
